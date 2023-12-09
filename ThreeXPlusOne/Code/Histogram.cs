21212b9329c1be356f2c649aa737de024855f067
wdthem <wdthem@gmail.com>
2023-12-10 09:45:19 +1300

    Add title to histogram

--- ThreeXPlusOne/Code/Histogram.cs
+++ ThreeXPlusOne/Code/Histogram.cs
@@ -15,7 +15,7 @@ internal static class Histogram
 
         var digitCounts = GenerateHistogramData(seriesData);
 
-        DrawHistogram(canvas, digitCounts, width, height);
+        DrawHistogram(canvas, digitCounts, width, height, "Numbers in series starting from 1-9");
 
         using SKImage image = SKImage.FromBitmap(bitmap);
         using SKData data = image.Encode(SKEncodedImageFormat.Png, 100);
@@ -45,23 +45,26 @@ internal static class Histogram
         return digitCounts;
     }
 
-    private static void DrawHistogram(SKCanvas canvas, List<int> counts, int canvasWidth, int canvasHeight)
+    private static void DrawHistogram(SKCanvas canvas, List<int> counts, int canvasWidth, int canvasHeight, string title)
     {
         int numberOfBars = counts.Count;
         int spacing = 10; // Spacing between bars
         int totalSpacing = spacing * (numberOfBars - 1); // Total spacing for all gaps
 
-        // Reserve space for Y-axis and X-axis labels
+        // Reserve space for Y-axis, X-axis labels, and the title
         int yAxisLabelSpace = 50; // Adjust this as needed
         int xAxisLabelHeight = 20; // Space for X-axis labels
+        int titleHeight = 30; // Space for the title
         int topPadding = 30; // Additional space at the top
+
+        int effectiveCanvasHeight = canvasHeight - titleHeight; // Adjust height for title space
 
         int barWidth = (canvasWidth - totalSpacing - yAxisLabelSpace) / numberOfBars;
         int maxCount = counts.Count > 0 ? counts.Max() : 1;
 
         // Scale the bars to leave space for the count text
         int adjustedMaxCount = maxCount + (maxCount / 10); // Adjust for space above the tallest bar
-        float scaleFactor = (float)(canvasHeight - xAxisLabelHeight - topPadding) / adjustedMaxCount;
+        float scaleFactor = (float)(effectiveCanvasHeight - xAxisLabelHeight - topPadding) / adjustedMaxCount;
 
         // Determine the number of Y-axis labels (every 500 units)
         int maxYAxisValue = ((adjustedMaxCount + 499) / 500) * 500;
@@ -73,7 +76,7 @@ internal static class Histogram
             int labelValue = i * 500;
             if (labelValue <= adjustedMaxCount) // Ensure label doesn't exceed the adjusted max
             {
-                float y = (canvasHeight - xAxisLabelHeight - topPadding) - (labelValue * scaleFactor);
+                float y = (effectiveCanvasHeight - xAxisLabelHeight - topPadding) - (labelValue * scaleFactor);
 
                 canvas.DrawLine(yAxisLabelSpace, y, canvasWidth, y, new SKPaint { Color = SKColors.Gray, IsAntialias = true, StrokeWidth = 1 });
                 canvas.DrawText(
@@ -89,11 +92,11 @@ internal static class Histogram
         {
             int count = counts[i];
             int barHeight = (int)(count * scaleFactor);
-            SKRect bar = new SKRect(
+            var bar = new SKRect(
                 yAxisLabelSpace + i * barWidth + i * spacing, // Adjust position for spacing and Y-axis labels
-                (canvasHeight - xAxisLabelHeight - topPadding) - barHeight,
+                (effectiveCanvasHeight - xAxisLabelHeight - topPadding) - barHeight,
                 yAxisLabelSpace + (i + 1) * barWidth + i * spacing, // Adjust position for spacing and Y-axis labels
-                canvasHeight - xAxisLabelHeight);
+                effectiveCanvasHeight - xAxisLabelHeight);
 
             canvas.DrawRect(bar, new SKPaint { Color = SKColors.Blue, IsAntialias = true });
 
@@ -101,12 +104,12 @@ internal static class Histogram
             canvas.DrawText(
                 (i + 1).ToString(),
                 yAxisLabelSpace + i * barWidth + i * spacing + barWidth / 2, // Adjust position for spacing and Y-axis labels
-                canvasHeight - 5, // Position below the graph
+                effectiveCanvasHeight - 5, // Position below the graph
                 new SKPaint { Color = SKColors.Black, IsAntialias = true, TextAlign = SKTextAlign.Center });
 
             // Draw bar totals inside the bars near the top
-            SKPaint textPaint = new SKPaint { Color = SKColors.White, IsAntialias = true, TextAlign = SKTextAlign.Center };
-            float textYPosition = (canvasHeight - xAxisLabelHeight - topPadding) - barHeight + 15; // Adjust for the height of the bar
+            var textPaint = new SKPaint { Color = SKColors.White, IsAntialias = true, TextAlign = SKTextAlign.Center };
+            float textYPosition = (effectiveCanvasHeight - xAxisLabelHeight - topPadding) - barHeight + 15; // Adjust for the height of the bar
             if (barHeight > 30) // Check if there's enough space to draw inside the bar
             {
                 canvas.DrawText(
@@ -116,5 +119,13 @@ internal static class Histogram
                     textPaint);
             }
         }
+
+        // Draw the title below the X-axis labels
+        var titlePaint = new SKPaint { Color = SKColors.Black, IsAntialias = true, TextAlign = SKTextAlign.Center, TextSize = 20 };
+        canvas.DrawText(
+            title,
+            canvasWidth / 2,
+            canvasHeight - 10, // Position at the bottom
+            titlePaint);
     }
 }
