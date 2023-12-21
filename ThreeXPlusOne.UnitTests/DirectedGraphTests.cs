62b6742b9db556244c534e30b6a6763d39e37b9e
wdthem <wdthem@gmail.com>
2023-12-21 20:28:37 +1300

    add unit test

---
+++ ThreeXPlusOne.UnitTests/DirectedGraphTests.cs
@@ -0,0 +1,35 @@
+using FluentAssertions;
+using Microsoft.Extensions.Options;
+using Moq;
+using ThreeXPlusOne.Code.Graph;
+using ThreeXPlusOne.Code.Interfaces;
+using ThreeXPlusOne.Config;
+using Xunit;
+
+namespace ThreeXPlusOne.UnitTests;
+
+public class DirectedGraphTests
+{
+    private readonly Mock<IFileHelper> _fileHelperMock;
+    private readonly IOptions<Settings> _settings = new OptionsWrapper<Settings>
+    (
+        new Settings { }
+    );
+
+    public DirectedGraphTests()
+    {
+        _fileHelperMock = new Mock<IFileHelper>();
+    }
+
+    [Fact]
+    public void AddSeries_Success00()
+    {
+        // Arrange
+        List<int> series = [64, 32, 16, 8, 4, 2, 1];
+
+        TwoDimensionalDirectedGraph twoDimensionalGraph = new(_settings, _fileHelperMock.Object);
+
+        // Act + Assert
+        twoDimensionalGraph.Invoking(graph => graph.AddSeries(series)).Should().NotThrow();
+    }
+}
