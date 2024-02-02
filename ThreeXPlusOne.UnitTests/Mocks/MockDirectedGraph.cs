bef43ead8f6e4a445fcafaa3f695f84803ebfcc7
wdthem <wdthem@gmail.com>
2024-02-03 10:58:45 +1300

    add a unit test regarding algorithm input

--- ThreeXPlusOne.UnitTests/Mocks/MockDirectedGraph.cs
+++ ThreeXPlusOne.UnitTests/Mocks/MockDirectedGraph.cs
@@ -31,11 +31,12 @@ public class MockDirectedGraph(IOptions<Settings> settings,
     {
     }
 
+#pragma warning disable CA1822 // Mark members as static
     public (double x, double y) RotateNode_Base(int nodeValue, double rotationAngle, double x, double y)
-
     {
         return NodeAesthetics.RotateNode(nodeValue, rotationAngle, x, y);
     }
+#pragma warning restore CA1822 // Mark members as static
 
     public void DrawDirectedGraph_Base()
     {
