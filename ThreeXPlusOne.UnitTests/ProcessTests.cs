030b1b25af9edf53efccd9d2f71f359787fc4a6b
wdthem <wdthem@gmail.com>
2024-05-19 16:22:15 +1200

    rename properties in unit tests

--- ThreeXPlusOne.UnitTests/ProcessTests.cs
+++ ThreeXPlusOne.UnitTests/ProcessTests.cs
@@ -16,22 +16,22 @@ public class ProcessTests
         new AppSettings { }
     );
 
-    private readonly Mock<IAlgorithmService> _algorithmMock;
+    private readonly Mock<IAlgorithmService> _algorithmServiceMock;
     private readonly Mock<IDirectedGraph> _directedGraph;
     private readonly IEnumerable<IDirectedGraph> _directedGraphs;
-    private readonly Mock<IHistogramService> _histogramMock;
-    private readonly Mock<IMetadataService> _metadataMock;
+    private readonly Mock<IHistogramService> _histogramServiceMock;
+    private readonly Mock<IMetadataService> _metadataServiceMock;
     private readonly Mock<IFileService> _fileServiceMock;
     private readonly Mock<IConsoleService> _consoleServiceMock;
 
     public ProcessTests()
     {
-        _algorithmMock = new Mock<IAlgorithmService>();
+        _algorithmServiceMock = new Mock<IAlgorithmService>();
         _directedGraph = new Mock<IDirectedGraph>();
         _directedGraph.Setup(graph => graph.Dimensions).Returns(2);
         _directedGraphs = [_directedGraph.Object];
-        _histogramMock = new Mock<IHistogramService>();
-        _metadataMock = new Mock<IMetadataService>();
+        _histogramServiceMock = new Mock<IHistogramService>();
+        _metadataServiceMock = new Mock<IMetadataService>();
         _fileServiceMock = new Mock<IFileService>();
         _consoleServiceMock = new Mock<IConsoleService>();
     }
@@ -54,14 +54,14 @@ public class ProcessTests
         // Arrange
         ResetSettings();
 
-        _algorithmMock.Setup(algorithm => algorithm.Run(It.IsAny<List<int>>())).Returns([[32, 16, 8, 4, 2, 1]]);
+        _algorithmServiceMock.Setup(algorithm => algorithm.Run(It.IsAny<List<int>>())).Returns([[32, 16, 8, 4, 2, 1]]);
         _consoleServiceMock.Setup(consoleService => consoleService.ReadYKeyToProceed("Generate 2D visualization?")).Returns(true);
 
         var process = new Process(_appSettings,
-                                  _algorithmMock.Object,
+                                  _algorithmServiceMock.Object,
                                   _directedGraphs,
-                                  _histogramMock.Object,
-                                  _metadataMock.Object,
+                                  _histogramServiceMock.Object,
+                                  _metadataServiceMock.Object,
                                   _fileServiceMock.Object,
                                   _consoleServiceMock.Object);
 
@@ -69,14 +69,14 @@ public class ProcessTests
         process.Run([]);
 
         // Assert
-        _algorithmMock.Verify(algorithm => algorithm.Run(It.IsAny<List<int>>()), Times.Once);
+        _algorithmServiceMock.Verify(algorithm => algorithm.Run(It.IsAny<List<int>>()), Times.Once);
         _directedGraph.Verify(graph => graph.AddSeries(It.IsAny<List<List<int>>>()), Times.AtLeastOnce);
         _directedGraph.Verify(graph => graph.PositionNodes(), Times.Once);
         _directedGraph.Verify(graph => graph.SetNodeAesthetics(), Times.Once);
         _directedGraph.Verify(graph => graph.SetCanvasDimensions(), Times.Once);
         _directedGraph.Verify(graph => graph.Draw(), Times.Once);
-        _histogramMock.Verify(histogram => histogram.GenerateHistogram(It.IsAny<List<List<int>>>()), Times.Once);
-        _metadataMock.Verify(metadata => metadata.GenerateMedatadataFile(It.IsAny<List<List<int>>>()), Times.Once);
+        _histogramServiceMock.Verify(histogram => histogram.GenerateHistogram(It.IsAny<List<List<int>>>()), Times.Once);
+        _metadataServiceMock.Verify(metadata => metadata.GenerateMedatadataFile(It.IsAny<List<List<int>>>()), Times.Once);
         _consoleServiceMock.Verify(helper => helper.ReadYKeyToProceed(It.IsAny<string>()), Times.Exactly(2));
         _fileServiceMock.Verify(helper => helper.WriteSettingsToFile(It.IsAny<bool>()), Times.Once);
         _consoleServiceMock.Verify(helper => helper.WriteSettingsSavedMessage(It.IsAny<bool>()), Times.Once);
@@ -95,10 +95,10 @@ public class ProcessTests
         _appSettings.Value.AlgorithmSettings.NumbersToExclude = "5,6,7";
 
         var process = new Process(_appSettings,
-                                  _algorithmMock.Object,
+                                  _algorithmServiceMock.Object,
                                   _directedGraphs,
-                                  _histogramMock.Object,
-                                  _metadataMock.Object,
+                                  _histogramServiceMock.Object,
+                                  _metadataServiceMock.Object,
                                   _fileServiceMock.Object,
                                   _consoleServiceMock.Object);
 
