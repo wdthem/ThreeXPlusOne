e04d732aeb0b44f066bfe15c3189966f8cb35313
wdthem <wdthem@gmail.com>
2024-05-25 10:28:38 +1200

    add more missing comments

--- ThreeXPlusOne/App/Interfaces/Services/ISingletonService.cs
+++ ThreeXPlusOne/App/Interfaces/Services/ISingletonService.cs
@@ -1,5 +1,8 @@
 namespace ThreeXPlusOne.App.Interfaces.Services;
 
+/// <summary>
+/// Empty interface implemented by classes that should in configured in the DI container as Singleton services
+/// </summary>
 public interface ISingletonService
 {
 }
